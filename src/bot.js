f3b3894f696fb61936811a59b8ebe29a9970b49c
CoelhoLado <naac.1999@gmail.com>
2020-11-22 18:17:08 +0000

    wrking bot command and api access

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,7 @@
 const Discord = require('discord.js');
-var read_cfg = require('./read_cfg')
+const fetch = require('node-fetch');
+const { constants } = require('buffer');
+const read_cfg = require('./read_cfg')
 
 const client = new Discord.Client();
 
@@ -7,4 +9,24 @@ client.once('ready', () => {
     console.log('Ready!');
 });
 
-client.login(read_cfg.getToken());
+client.login(read_cfg.getToken())
+
+client.on('message', message => {
+    if (!message.content.startsWith(read_cfg.getPrefix()) || message.author.bot) return;
+    var command = message.content.substring(1)
+
+    console.log('[RECIEVED]' + command)
+
+    var args = command.split(/ +/)
+
+    if(command == "ping"){
+        message.channel.send('pong.')
+    }
+
+    if((/pokemon [a-zA-Z0-9 ]+/).test(command)){
+        fetch('https://pokeapi.co/api/v2/pokemon/' + args[1])
+            .then(response => response.json())
+            .then(json => message.channel.send("Here is " + json.name, { files: [json.sprites["front_default"]] }));
+    }
+
+});
--- src/read_cfg.js
+++ src/read_cfg.js
@@ -8,3 +8,12 @@ exports.getToken = () => {
     return json.token
 
 }
+
+exports.getPrefix = () => {
+
+    var content = fs.readFileSync('discord.cfg')
+    var json = JSON.parse(content)
+
+    return json.prefix
+
+}
